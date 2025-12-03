e63f6480f76b88f5f1d7cefcdd48cf5655776b90
guidutra-china <67529834+guidutra-china@users.noreply.github.com>
2025-12-03 00:57:06 -0500

    Feature: Add enhanced global search to OrderResource with customer details and actions

--- app/Filament/Resources/Orders/OrderResource.php
+++ app/Filament/Resources/Orders/OrderResource.php
@@ -32,7 +32,35 @@ class OrderResource extends Resource
     protected static ?string $modelLabel = 'RFQ';
     protected static ?string $pluralModelLabel = 'RFQs';
 
-    
+    protected static ?string $recordTitleAttribute = 'order_number';
+
+    public static function getGlobalSearchResultTitle($record): string
+    {
+        return $record->order_number . ' - ' . ($record->customer?->name ?? 'No Customer');
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['order_number', 'customer_nr_rfq', 'customer.name', 'customer.email'];
+    }
+
+    public static function getGlobalSearchResultDetails($record): array
+    {
+        return [
+            'Customer' => $record->customer?->name ?? 'N/A',
+            'Status' => ucfirst($record->status),
+            'Customer Ref' => $record->customer_nr_rfq ?? 'N/A',
+        ];
+    }
+
+    public static function getGlobalSearchResultActions($record): array
+    {
+        return [
+            \Filament\Actions\Action::make('edit')
+                ->url(static::getUrl('edit', ['record' => $record]))
+                ->icon('heroicon-o-pencil'),
+        ];
+    }
 
     public static function form(Schema $schema): Schema
     {
