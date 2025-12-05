a4b74bb88ef1cd294fb428abb5ad989c6da55082
guidutra-china <67529834+guidutra-china@users.noreply.github.com>
2025-12-04 23:20:00 -0500

    fix: remover uso desnecessário de repository no SuppliercontactsRelationManager
    ERRO CORRIGIDO:
    - Call to undefined method SupplierRepository::getContactsQuery()
    
    CAUSA:
    - Mesmo problema do ClientcontactsRelationManager
    - Repository usage desnecessário
    - Método inexistente sendo chamado
    
    SOLUÇÃO:
    - Removido SupplierRepository e método mount()
    - Removida chamada ->query() desnecessária
    - RelationManager usa relação 'suppliercontacts' automaticamente
    
    PADRÃO APLICADO:
    - Mesma correção aplicada em ClientcontactsRelationManager
    - Código idiomático do Filament 4
    - Sem dependências desnecessárias

--- app/Filament/Resources/Suppliers/RelationManagers/SuppliercontactsRelationManager.php
+++ app/Filament/Resources/Suppliers/RelationManagers/SuppliercontactsRelationManager.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources\Suppliers\RelationManagers;
 
 use App\Filament\Resources\SupplierContacts\Schemas\SupplierContactForm;
 use App\Filament\Resources\SupplierContacts\Tables\SupplierContactsTable;
-use App\Repositories\SupplierRepository;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
 use Filament\Tables\Table;
@@ -20,13 +19,6 @@ class SuppliercontactsRelationManager extends RelationManager
 
     protected static ?string $title = 'Contacts';
 
-    protected SupplierRepository $repository;
-
-    public function mount(): void {
-        parent::mount();
-        $this->repository = app(SupplierRepository::class);
-    }
-
     public function form(Schema $schema): Schema
     {
         return SupplierContactForm::configure($schema);
@@ -35,9 +27,6 @@ class SuppliercontactsRelationManager extends RelationManager
     public function table(Table $table): Table
     {
         return SupplierContactsTable::configure($table)
-            ->query(
-                $this->repository->getContactsQuery($this->getOwnerRecord()->id)
-            )
             ->headerActions([
                 CreateAction::make(),
             ])
